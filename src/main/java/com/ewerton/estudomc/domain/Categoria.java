41a4146b71f3632ed32aa01471fb406f0ef03ace
Ewerton Campos <ewertoncampos@gmail.om>
2019-10-03 23:05:49 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/ewerton/estudomc/domain/Categoria.java
+++ src/main/java/com/ewerton/estudomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.ewerton.estudomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 //Intrface informa objetos pode ser convertido em bytes
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	//Atributos Basico
 	private Integer id;
 	private String nome;
